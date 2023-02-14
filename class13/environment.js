b71b88d9b8bbc72a7a73a206278ff25dfc168f78
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2023-02-14 22:05:47 +0100

    Exercises about while, do and for in Javascript

---
+++ class13/environment.js
@@ -0,0 +1,16 @@
+var c = 1;
+while (c <= 6) {
+    console.log(`Step ${c}`);
+    c += 1
+}
+
+do {
+    console.log(`Step ${c}`);
+    c -= 1;
+} while (c >= 1);
+
+
+for (var c=1; c<=10; c += 1) {
+    console.log(`Step ${c}`);
+}
+
---
+++ class13/environment2.js
@@ -0,0 +1,5 @@
+console.log('Starting...');
+for (var c=1; c<=10; c++) {
+    console.log(c);
+}
+console.log('End');
